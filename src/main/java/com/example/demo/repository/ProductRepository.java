3f785853b2ffb854444b9c32dec9304381cde10b
Dipen Boro <borod9200@gmail.com>
2026-05-08 09:38:00 +0530

    init

---
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Product;
+/*
+  creating an interface ProductRepority that extends JpaRespository
+  JpaRepository := Provies all necessary CRUD methods (save(), findAll(), findById())
+  @Repository := Marks this interface as a string data reposirotry. enabling automatci CRUD operation
+
+*/
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {}
